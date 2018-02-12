1d7a3805993ad43065af37d1d59baa2bec5d68b7
Chris Clark <forclarkemail@gmail.com>
2018-02-12 06:38:24 -0500

    node working basis

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var http = require('http');
+if(process.env.LOCAL !== true){
+	require('dotenv').load();
+}
+
 var express = require('express');
 var routes = require('./app/routes/index.js');
 var session = require('express-session');
@@ -8,17 +11,16 @@ var pg = require('pg');
 var pgSession = require('connect-pg-simple')(session);
 var parse = require('pg-connection-string').parse;
 const fs = require('fs');
+var http = require('http');
 
-// var passport = require('passport');
-
-var dsConfig = "./app/config/config.yml";
+var passport = require('passport');
 
 //postgresql config
 var config = parse(process.env.DATABASE_URL);
 config.ssl = true;
 
 var app = express();
-// require('./app/config/passport')(passport);
+require('./app/config/passport')(passport);
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -36,25 +38,26 @@ app.use(session({
 	saveUninitialized: false
 }));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-routes(app);
-// routes(app, passport);
+// routes(app);
+routes(app, passport);
 
 const server = http.createServer(app);
 
+// var dsConfig = "./app/config/config.yml";
 // const Deepstream = require('deepstream.io')
 // const ds = new Deepstream(dsConfig);
 // // var deepstreamC = require('deepstream.io-client-js');
 // // const client = deepstreamC('ws://localhost:6020').login({username: "server"});
 // ds.start();
+
 var port;
 if(process.env.LOCAL == false){
 	port = '/tmp/nginx.socket';
 }else{
-	port = 8080;
-}
+	port = 8080;}
 
 server.listen(port, function () {
 	console.log('Node.js listening on port ' + port + '...');
